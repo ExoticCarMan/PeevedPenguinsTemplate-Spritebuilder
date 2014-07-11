30ab9ce701511da1360a9260a675a56ae59bed4c
ExoticCarMan <nathancarguy@gmail.com>
2014-07-10 23:25:22 -0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -12,31 +12,31 @@
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
 }
-
-//called when CCB file loaded
-- (void)didLoadFromCCB {
-    //tell scene to accept touches
-    self.userInteractionEnabled = true;
-}
-
-//called for every touch in scene
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-    [self launchPenguin];
-}
-
-- (void)launchPenguin {
-    //load Penguin.ccb setup in Spritebuilder
-    CCNode* penguin = [CCBReader load:@"Penguim"];
-    //position penguin at bowl of catapult
-    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
-    
-    //add penguin to physicsNode of scene (because it has physics enabled)
-    [_physicsNode addChild:penguin];
-    
-    //manually create and apply launch force to penguin
-    CGPoint launchDirection = ccp(1, 0);
-    CGPoint force = ccpMult(launchDirection, 8000);
-    [penguin.physicsBody applyForce:force];
-}
+//
+////called when CCB file loaded
+//- (void)didLoadFromCCB {
+//    //tell scene to accept touches
+//    self.userInteractionEnabled = true;
+//}
+//
+////called for every touch in scene
+//- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+//    [self launchPenguin];
+//}
+//
+//- (void)launchPenguin {
+//    //load Penguin.ccb setup in Spritebuilder
+//    CCNode* penguin = [CCBReader load:@"Penguim"];
+//    //position penguin at bowl of catapult
+//    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
+//    
+//    //add penguin to physicsNode of scene (because it has physics enabled)
+//    [_physicsNode addChild:penguin];
+//    
+//    //manually create and apply launch force to penguin
+//    CGPoint launchDirection = ccp(1, 0);
+//    CGPoint force = ccpMult(launchDirection, 8000);
+//    [penguin.physicsBody applyForce:force];
+//}
 
 @end
